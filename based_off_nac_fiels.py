ec88f0857ccbd2d48a1d43cd7a164f2652c3828b
cdcox <cdcox1@gmail.com>
2015-05-27 10:48:39 -0700

    Put the loop inside to do local areas

--- based_off_nac_fiels.py
+++ based_off_nac_fiels.py
@@ -90,92 +90,83 @@ for filen in f_list:
     freq_steps=2
     time_cap=300
     #Cutting areea and incidence of SWR
-    led=butter_bandpass_filter(zed, 0.1, 45, fs, 4,'lower')
-    down=(led<(np.mean(led)-2*np.std(led)))
-    target_list=np.where(down)
-    starts,stops,area_list=build_starts_stops(led,target_list)
-    meaner=np.mean(led)
-    for sN in range(len(starts)):
-        temp_start=[]
-        temp_stop=[]
-        move=0
-        check=1
-        while check!=0:
-            q_check=led[starts[sN]-move]
-            if q_check>meaner:
-                check=0
-                temp_start.append(starts[sN]-move)
-                ttstart=starts[sN]-move
-            else:
-                move+=1
-        print 'check'+str(sN)+'out of'+str(np.max(starts))
-        move=0
-        check=1
-        while check!=0:
-            if stops[sN]+move==len(led):
-                check=0
-                temp_stop.append(stops[sN]+move)
-                ttstop=stops[sN]+move
-            else:
-                q_check=led[stops[sN]+move]
-            if q_check>meaner:
-                check=0
-                temp_stop.append(stops[sN]+move)
-                ttstop=stops[sN]+move
-            else:
-                move+=1
-        if np.sum(led[ttstart:ttstop])>0:
-            print 'haters'
-        area_list.append(np.sum(led[ttstart:ttstop]))
-    #Cutting out max height and local frequency of SWR
-    ked=butter_bandpass_filter(zed, lowcut, highcut, fs, 2,'band')
-    down=(ked<(np.mean(ked)-3*np.std(ked)))
-    target_list=np.where(down)
-    starts,stops,area_listzed=build_starts_stops(ked,target_list)
-    max_list=[]
-    for sN in range(len(starts)):
-        temp_start=[]
-        temp_stop=[]
-        start=starts[sN]-15/1000*fs
-        if start<0:
-            start=0
-        stop=stops[sN]+15/1000*fs
-        if stop> len(ked):
-            stop=len(ked)-1
-        power_kind_of=np.max(ked[start:stop])
-    #OK work this in a second
-        '''
-        L = len(ked[start:stop])
-        fftdata = fft(ked[start:stop])
-        dt = 1/fs 
-        w=fftfreq(L,dt) 
-        ipos = where(w>0)
-        freqs = w[ipos]        # only look at positive frequencies
-        mags = abs(fftdata[ipos])
-        '''
-        max_list.append(power_kind_of)
-    master_max.append(max_list)
-    master_area.append(area_list)
-    master_starts.append(starts)
-    master_stops.append(stops)
-    output_len=output_length*fs
+    old_led=butter_bandpass_filter(zed, 0.1, 45, fs, 4,'lower')
+    old_ked=butter_bandpass_filter(zed, lowcut, highcut, fs, 5,'band')
     epochs=int(np.ceil(len(zed)/output_len))
     frequency=[]
     avg_areas=[]
     power_kind=[]
     for eN in range(1,epochs):
-        try:
-            print eN
-            mask_range=(np.array(starts)<(eN*output_len))*(np.array(starts)>((eN-1)*output_len))
-            valid_areas=np.array(area_list)[mask_range]
-            valid_powers=np.array(max_list)[mask_range]
-            frequency.append(len(valid_areas)/output_length)
-            avg_areas.append(median(valid_areas))
-            power_kind.append(median(valid_powers))
-        except:
-            frequency.append(0)
-            avg_areas.append(0)
-            power_kind.append(0)
+        ked=old_ked[(eN-1)*output_len:eN*output_len]
+        led=old_led[(eN-1)*output_len:eN*output_len]
+        down=(led<(np.mean(led)-2*np.std(led)))
+        target_list=np.where(down)
+        starts,stops,area_list=build_starts_stops(led,target_list)
+        meaner=np.mean(led)
+        for sN in range(len(starts)):
+            temp_start=[]
+            temp_stop=[]
+            move=0
+            check=1
+            while check!=0:
+                q_check=led[starts[sN]-move]
+                if q_check>meaner:
+                    check=0
+                    temp_start.append(starts[sN]-move)
+                    ttstart=starts[sN]-move
+                else:
+                    move+=1
+            print 'check'+str(sN)+'out of'+str(np.max(starts))
+            move=0
+            check=1
+            while check!=0:
+                if stops[sN]+move==len(led):
+                    check=0
+                    temp_stop.append(stops[sN]+move)
+                    ttstop=stops[sN]+move
+                else:
+                    q_check=led[stops[sN]+move]
+                if q_check>meaner:
+                    check=0
+                    temp_stop.append(stops[sN]+move)
+                    ttstop=stops[sN]+move
+                else:
+                    move+=1
+            if np.sum(led[ttstart:ttstop])>0:
+                print 'haters'
+            area_list.append(np.sum(led[ttstart:ttstop]))
+        #Cutting out max height and local frequency of SWR
+        ked=butter_bandpass_filter(zed, lowcut, highcut, fs, 2,'band')
+        down=(ked<(np.mean(ked)-3*np.std(ked)))
+        target_list=np.where(down)
+        starts,stops,area_listzed=build_starts_stops(ked,target_list)
+        max_list=[]
+        for sN in range(len(starts)):
+            temp_start=[]
+            temp_stop=[]
+            start=starts[sN]-15/1000*fs
+            if start<0:
+                start=0
+            stop=stops[sN]+15/1000*fs
+            if stop> len(ked):
+                stop=len(ked)-1
+            power_kind_of=np.max(ked[start:stop])
+        #OK work this in a second
+            '''
+            L = len(ked[start:stop])
+            fftdata = fft(ked[start:stop])
+            dt = 1/fs 
+            w=fftfreq(L,dt) 
+            ipos = where(w>0)
+            freqs = w[ipos]        # only look at positive frequencies
+            mags = abs(fftdata[ipos])
+            '''
+        max_list.append(power_kind_of)
+        power_kind.append(max_list)
+        frequency.append(len(area_list)/output_length)
+        avg_areas.append(np.median(area_list))
+        master_starts.append(starts)
+        master_stops.append(stops)
     master_power_real.append(power_kind)
     master_freq.append(frequency)
     master_median.append(avg_areas)

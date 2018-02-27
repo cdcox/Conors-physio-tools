4557caafa6034f98c5158f162fa67843b16dd1cf
cdcox <cdcox1@gmail.com>
2018-02-27 11:08:57 -0800

    Adding new files for frequce hunting and fft plotting

--- new_band_pass_forxlfreuq.py
+++ new_band_pass_forxlfreuq.py
@@ -26,8 +26,8 @@ class AutoVivification(dict):
             value = self[item] = type(self)()
             return value
 
-def power_plot(fs,input_array):   
-    nyquist = fs/2 #1
+def power_plot(zs,input_array):   
+    nyquist = zs/2 #1
     fSpaceSignal = np.fft.fft(input_array)/len(input_array) #2
     fBase = np.linspace(0,nyquist,np.floor(len(input_array)/2)+1) #3
     powerPlot = plt.subplot(111)
@@ -35,10 +35,11 @@ def power_plot(fs,input_array):
     complexConjugate = np.conj(halfTheSignal)#6
     powe = halfTheSignal*complexConjugate#7
     powerPlot.plot(fBase,np.log(powe), c='k',lw=2) #8
-    powerPlot.set_xlim([0, 2000]); #9
-    powerPlot.set_xticks(range(0,2000,5));#10
+    powerPlot.set_xlim([0, 100]); #9
+    powerPlot.set_xticks(range(0,100,5));#10
     powerPlot.set_xlabel('Frequency (in Hz)') #11
     powerPlot.set_ylabel('Power')#
+    return powe.real,fBase
 
 def read_and_parse(directory):
     file_to_open=os.path.join(directory,'files_to_be_run.csv')
@@ -77,15 +78,8 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=8):
     y = filtfilt(b, a, data)
     return y
 
-def calculate_stuff_about_spikes(starts_stops,out_traits):
-    
-    for spike in starts_stops:
-        base_spike_data = bb_filt_out[spike[0]-15:spike[0]+15]
-        traits=[np.max(base_spike_data[:15]),base_spike_data[15],np.max(base_spike_data[:15]),np.max(base_spike_data[:15])-base_spike_data[15]]
-        out_traits.append(traits)
-    return out_traits
-
-directory = r'C:\Users\colorbox\Documents\ben_data\playtime'
+        
+directory = r'C:\Users\colorbox\Documents\ben_data\Optical_stim'
 
 fs = 20000
 highcut = 3000
@@ -93,17 +87,16 @@ lowcut = 300
 dir_list = os.listdir(directory)
 all_out_names = []
 all_out_histograms = []
-out_traits=[]
-all_spikes=[]
+cit=[]
 #dir_list=dir_list[0:2]
 outputter=AutoVivification()
+nems=[]
 for book_name in dir_list:
     if not('.xlsx' in book_name[-5:]):
         continue
     data_book=xlrd.open_workbook(os.path.join(directory,book_name))
     snames=data_book.sheet_names()
-    for thresholds in [-0.025]:
-
+    for thresholds in [-0.05]:
         for sheet_name in snames:
             j=0
             sheet=data_book.sheet_by_name(sheet_name)
@@ -122,37 +115,43 @@ for book_name in dir_list:
                 values=[x for x in values if x!='']
                 i+=1
                 bb_filt_out = butter_bandpass_filter(values,lowcut,highcut,fs,8)
-                plt.plot(bb_filt_out[0:10000],linewidth=.1)
-                starts_stops = generate_starts_stops(bb_filt_out[:],thresholds)
-                seconds = len(bb_filt_out[:])/fs
-                if len(starts_stops)==0:
-                    out_hist=list(np.zeros(np.round(seconds)))
-                else:
-                    out_hist = np.histogram(starts_stops[:,0],np.round(seconds))
-                    out_hist = list(out_hist[0])
-                    ssa=np.array(starts_stops)
-                    ssa=ssa[ssa[:,1]<10000,:].tolist()
-                    maxbb=np.max(bb_filt_out)
-                    minbb=np.min(bb_filt_out)
-                    for pairs in ssa:
-                        plt.plot([pairs[0],pairs[0]],[minbb,maxbb],'-r',lw=.1)
-                        plt.plot([pairs[1],pairs[1]],[minbb,maxbb],'-b',lw=.1)
-                for ii in range(len(starts_stops)):
-                    all_spikes.append(list(bb_filt_out[starts_stops[ii,:][0]-15:starts_stops[ii,:][0]+15]))
-                out_traits=calculate_stuff_about_spikes(starts_stops,out_traits)
-                
-                all_out_names.append(book_name+sheet_name+str(j)+'_'+str(i)+names)
-                all_out_histograms.append(out_hist)
-                sname=sheet_name.replace('/','')
-                names=names.replace('/','')
-                axes = plt.gca()
-                axes.set_ylim([-0.2,0.2])
-                plt.savefig(os.path.join(directory,book_name)+str(thresholds)+sname+str(j)+'_'+str(i)+names+'.png',dpi=900)
-                plt.cla()
-                plt.clf()
-                outputter[thresholds][book_name+'_'+sheet_name+str(j)+'_'+str(i)]=[names,out_hist]
-                print(sheet_name+' '+book_name+' '+str(j)+'_'+str(i)+'fail')
-            print(sheet_name+' '+book_name)
+                #plt.plot(bb_filt_out[0:10000],linewidth=.1)
+                epochs=int(np.floor(len(bb_filt_out)/fs/2.5))
+                print(epochs)
+                for jjj in range(epochs-1):
+                    epoched=bb_filt_out[jjj*fs*2.5:(jjj+1)*fs*2.5]
+                    cut=epoched*(epoched<thresholds)
+                    powe,fbase=power_plot(fs,cut)
+                    cit.append(powe)
+                    '''
+                    starts_stops = generate_starts_stops(bb_filt_out[:],thresholds)
+                    seconds = len(bb_filt_out[:])/fs
+                    if len(starts_stops)==0:
+                        out_hist=list(np.zeros(np.round(seconds)))
+                    else:
+                        out_hist = np.histogram(starts_stops[:,0],np.round(seconds))
+                        out_hist = list(out_hist[0])
+                        ssa=np.array(starts_stops)
+                        ssa=ssa[ssa[:,1]<10000,:].tolist()
+                        maxbb=np.max(bb_filt_out)
+                        minbb=np.min(bb_filt_out)
+                        for pairs in ssa:
+                            plt.plot([pairs[0],pairs[0]],[minbb,maxbb],'-r',lw=.1)
+                            plt.plot([pairs[1],pairs[1]],[minbb,maxbb],'-b',lw=.1)
+                    all_out_names.append(book_name+sheet_name+str(j)+'_'+str(i)+names)
+                    all_out_histograms.append(out_hist)
+                    sname=sheet_name.replace('/','')
+                    names=names.replace('/','')
+                    axes = plt.gca()
+                    axes.set_ylim([-0.2,0.2])
+                    plt.savefig(os.path.join(directory,book_name)+str(thresholds)+sname+str(j)+'_'+str(i)+names+'.png',dpi=900)
+                    plt.cla()
+                    plt.clf()
+                    outputter[thresholds][book_name+'_'+sheet_name+str(j)+'_'+str(i)]=[names,out_hist]
+                    print(sheet_name+' '+book_name+' '+str(j)+'_'+str(i)+'fail')
+                    '''
+                    print(sheet_name+' '+book_name+names+str(jjj))
+                    nems.append(names+str(jjj))
 '''
 out_book=xlwt.Workbook(encoding="utf-8")
 new_threshs=list(outputter.keys())
